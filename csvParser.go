c05a411fef9240caa426b2428f5e434c73d14d7f
Slavomir <gagliardetto@users.noreply.github.com>
2015-11-25 01:33:51 +0100

    code refactoring

--- csvParser.go
+++ csvParser.go
@@ -11,7 +11,7 @@ import (
 
 //Parser parse a csv file and returns an array of pointers of the type specified
 type Parser interface {
-	Parse(outType interface{})
+	Parse(resultType interface{})
 }
 
 //CsvParser parses a csv file and returns an array of pointers the type specified
@@ -24,51 +24,67 @@ type CsvParser struct {
 
 //Parse creates the array of the given type from the csv file
 func (parser CsvParser) Parse(f interface{}) ([]interface{}, error) {
-	var lines, err = getCsvLines(parser.CsvFile, parser.CsvSeparator)
+
+	file, err := os.Open(parser.CsvFile)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	var csvReader = csv.NewReader(file)
+	csvReader.Comma = parser.CsvSeparator
+	///
 
 	if err != nil {
 		return nil, err
 	}
 
-	var result = make([]interface{}, 0, 0)
+	var results = make([]interface{}, 0, 0)
 
-	fn := reflect.ValueOf(f)
-	outType := fn.Type()
+	resultType := reflect.ValueOf(f).Type()
 
-	var startIndex = 0
-	if parser.SkipFirstLine {
-		startIndex = 1
-	}
+	for {
 
-	for l := startIndex; l < len(lines); l++ {
-		//create the new item
-		var x = reflect.New(outType).Interface()
+		rawLine, err := csvReader.Read()
+		if err != nil {
+			if fmt.Sprint(err) == "EOF" {
+				break
+			} else {
+				return nil, fmt.Errorf("error: %v", err)
+			}
+		}
+
+		if parser.SkipFirstLine {
+			parser.SkipFirstLine = false
+			continue
+		}
+
+		var newItem = reflect.New(resultType).Interface()
 
 		// set all the struct fields
-		for i := 0; i < outType.NumField(); i++ {
-			var currentField = outType.Field(i)
+		for fieldIndex := 0; fieldIndex < resultType.NumField(); fieldIndex++ {
+			var currentField = resultType.Field(fieldIndex)
 
 			var csvTag = currentField.Tag.Get("csv")
-			var csvColumn, csvTagErr = strconv.Atoi(csvTag)
+			var csvColumnIndex, csvTagErr = strconv.Atoi(csvTag)
 
 			if csvTagErr != nil {
 				if csvTag == "" {
-					csvColumn = i
+					csvColumnIndex = fieldIndex
 				} else {
 					return nil, csvTagErr
 				}
 			}
 
-			if csvColumn < 0 {
+			if csvColumnIndex < 0 {
 				return nil, fmt.Errorf("csv tag in struct field %v is less than zero", currentField.Name)
 			}
 
-			if len(lines[l]) <= csvColumn {
-				return nil, fmt.Errorf("Trying to access csv column %v for field %v, but csv has only %v column", csvColumn, currentField.Name, len(lines[l]))
+			if csvColumnIndex >= len(rawLine) {
+				return nil, fmt.Errorf("Trying to access csv column %v for field %v, but csv has only %v column(s)", csvColumnIndex, currentField.Name, len(rawLine))
 			}
 
-			var csvElement = lines[l][csvColumn]
-			var settableField = reflect.ValueOf(x).Elem().FieldByName(currentField.Name)
+			var csvElement = rawLine[csvColumnIndex]
+			var settableField = reflect.ValueOf(newItem).Elem().FieldByName(currentField.Name)
 
 			if csvElement == "" && parser.SkipEmptyValues {
 				continue
@@ -122,23 +138,8 @@ func (parser CsvParser) Parse(f interface{}) ([]interface{}, error) {
 				settableField.Set(reflect.ValueOf(date))
 			}
 		}
-		//append the result
-		result = append(result, x)
+
+		results = append(results, newItem)
 	}
-	return result, err
-}
-
-//GetCsvLines returns all the lines from a given csvfile using the given separator
-func getCsvLines(csvFile string, separator rune) ([][]string, error) {
-	file, err := os.Open(csvFile)
-	if err != nil {
-		return nil, err
-	}
-
-	defer file.Close()
-
-	var csvReader = csv.NewReader(file)
-	csvReader.Comma = separator
-
-	return csvReader.ReadAll()
+	return results, err
 }
